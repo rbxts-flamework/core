e46a3abc410161e22e5cbba9b15c5cbd59a4ce84
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2023-09-26 00:35:48 -0400

    Make the abstract constructor type abstract...

--- src/utility.ts
+++ src/utility.ts
@@ -1,5 +1,5 @@
 export type Constructor<T = object> = new (...args: never[]) => T;
-export type AbstractConstructor<T = object> = new (...args: never[]) => T;
+export type AbstractConstructor<T = object> = abstract new (...args: never[]) => T;
 
 export function isConstructor(obj: object): obj is Constructor {
 	return isAbstractConstructor(obj) && "new" in obj;
