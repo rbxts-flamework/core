06c2b57a34ec4bf2cb76c2ebf2f1a9731677d753
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2025-04-11 21:06:13 -0400

    Add transformer prelude

---
+++ src/prelude.ts
@@ -0,0 +1,9 @@
+/**
+ * This file contains preludes for the transformer.
+ *
+ * This is primarily necessary due to a bug with the `@rbxts/t` package.
+ */
+
+import { t } from "@rbxts/t";
+
+export { t };
